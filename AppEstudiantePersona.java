c489e5292714149c9ceaf2916e52d420e75b9b25
Orlando R <orlandodavid321.26@gmail.com>
2023-12-18 20:55:13 -0300

    ACTIVSESION08ORLANDO

---
+++ AppEstudiantePersona.java
@@ -0,0 +1,12 @@
+public class AppEstudiantePersona {
+    
+    public static void main(String[] args) {
+        
+        Estudiante e = new Estudiante("Luis Garcia", 20);
+        Persona p = new Persona("Marta Gomez", 20);
+
+        e.mostrarDatos();
+        p.mostrarDatos();
+
+    }
+}
